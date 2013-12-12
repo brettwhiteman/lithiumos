cb1245e87ec5cf9280f7fd414560934d7863d2d0
BrettMW <msea@gmx.com>
2013-12-13 00:26:50 +1300

    Improved print system call

--- kernel/process/syscall.c
+++ kernel/process/syscall.c
@@ -4,21 +4,12 @@
 
 #define FUNC_CODE_PRINT 		0x00000000
 
-struct printInfo
-{
-	byte attrib;
-	char *string;
-} __attribute__((packed));
-
 void call_handler(isr_t *stk)
 {
 	switch(stk->eax)
 	{
-	case FUNC_CODE_PRINT:;
-		byte oc = get_colour();
-		set_colour(((struct printInfo *)(stk->ebx))->attrib);
-		print_string(((struct printInfo *)(stk->ebx))->string);
-		set_colour(oc);
+	case FUNC_CODE_PRINT:
+		print_string((const char *)stk->ebx);
 		break;
 	}
 }
